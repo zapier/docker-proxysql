c988e900af45ca38927c0a53f45f2634688d246e
Ratnadeep Debnath <rtnpro@gmail.com>
2019-08-02 17:07:47 +0530

    Add logging formatter in proxysql metrics collection script.

--- metrics/proxysql.py
+++ metrics/proxysql.py
@@ -17,7 +17,10 @@ import pymysql.cursors
 from datadog.dogstatsd.base import DogStatsd
 
 logger = logging.getLogger()
-logger.addHandler(logging.StreamHandler())
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 logger.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
 
 GAUGE = "gauge"
